b157e926d38912b04fab92d9298b9ec5036987a6
drlecks <contacte@powdercode.com>
2022-12-12 17:09:28 +0100

    Accounts hash & sign fixes fot hexa input

--- Core/Accounts.php
+++ Core/Accounts.php
@@ -128,21 +128,19 @@ class Accounts
   
 
 	public static function hashMessage(string $message) : string
-	{ 
-		if (substr($message, 0, 2) == '0x') {
-			$removed_prefix = substr($message, 2);
-			if (ctype_xdigit($removed_prefix) && strlen($removed_prefix) % 2 == 0) {
-				$message = hex2bin($removed_prefix);
-			}
-		}
+	{  
+		if (substr($message, 0, 2) == '0x' && strlen($message) % 2 == 0 && ctype_xdigit(substr($message, 2))) {
+			$message = hex2bin(substr($message, 2));
+		} 
 
-		$msglen = strlen($message);
-		//"\x19Ethereum Signed Message:\n" + message.length + message and hashed using keccak256.
-		$msg    = hex2bin("19") . "Ethereum Signed Message:" . hex2bin("0A") . $msglen . $message;
+		$messagelen = strlen($message);
+		//"\x19Ethereum Signed Message:\n" + hash.length + hash and hashed using keccak256.
+		$msg    = hex2bin("19") . "Ethereum Signed Message:" . hex2bin("0A") . $messagelen . $message;
 		$hash   = Keccak::hash($msg, 256);
 
-		return $hash;
+		return $hash; 
 
+		//https://github.com/web3/web3.js/blob/v1.2.11/packages/web3-eth-accounts/src/index.js (hashMessage)
 		//web3.eth.accounts.hashMessage("Hello World")
  		//"0xa1de988600a42c4b4ab089b619297c17d53cffae5d5120d82d8a92d0bb3b78f2" 
 	}
@@ -165,11 +163,12 @@ class Accounts
 	{  
 		$hash   = self::hashMessage($message); 
 
-		if (ctype_xdigit($signature) && substr($signature, 0, 2) == '0x') {
+		if (substr($signature, 0, 2) == '0x') {
 			$signature = substr($signature, 2); 
 		} 
-		$sign   = ["r" => substr($signature, 2, 64), "s" => substr($signature, 66, 64)];
-		$recid  = ord(hex2bin(substr($signature, 130, 2))) - 27; 
+		
+		$sign   = ["r" => substr($signature, 0, 64), "s" => substr($signature, 64, 64)];
+		$recid  = ord(hex2bin(substr($signature, 128, 2))) - 27; 
   
 		if ($recid != ($recid & 1))  {
 			throw new Exception("Signature recovery not valid");
