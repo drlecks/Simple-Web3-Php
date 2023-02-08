cbdac6394fd81898d9158cfe7395c30895526611
Thai Phan Bui <phanthai12@gmail.com>
2023-02-08 22:51:00 +0700

    Bug fix with call function with 0, false
    when i call a function with `callData` is `0`, `false` or array has them, it will throw an error like below:
    `Undefined array key "id"`
    
    Reason: You using `==` with `null`
    Solution: change `==` to `===`

--- Core/ABI.php
+++ Core/ABI.php
@@ -229,7 +229,7 @@ class ABI
         if ($function === null || count($function->inputs) === 0)  {   
             $data = [];
         } 
-        else if ($data == null)  {
+        else if ($data === null)  {
             $data = [];
         } 
         else if(!is_array($data))  {
@@ -270,7 +270,7 @@ class ABI
         {     
             $var_name = is_object($input) ? $input->name : $input;
             $inputData = is_object($data) ? $data->$var_name : $data[$pos];  
-            if (is_array($data) && $inputData == null) $inputData = $data[$var_name];
+            if (is_array($data) && $inputData === null) $inputData = $data[$var_name];
 
             $hashData .= self::EncodeInput($input, $inputData, 1, $currentDynamicIndex); 
   
